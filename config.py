f45ed6886675b532c8819cde9170962d8cde4c21
Padam Sethia <padamsethia5@gmail.com>
2019-02-07 23:56:02 +0530

    Added WEBDRIVER_PATH

--- config.py
+++ config.py
@@ -9,3 +9,5 @@ SECRET_KEY = 'the_very_secure_secret_security_key_that_no_will_ever_guess'
 SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://root@127.0.0.1/automato'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 RABBITMQ_HOST = "localhost"
+
+WEBDRIVER_PATH = r"C:\Users\padam\Downloads\chromedriver_win32\chromedriver.exe"
