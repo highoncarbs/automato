bed62b723ada7006fdde07b7477b4eccbe3e8c21
sehe9001 <Hemant.Sethia.intern@nielsen.com>
2019-03-15 18:15:23 +0530

    Updated Model

--- model.py
+++ model.py
@@ -38,8 +38,47 @@ class template_form(FlaskForm):
     mssg_7 = StringField('mssg_7')
     mssg_8 = StringField('mssg_8')
 
-# DB models 
+########## DB models ############  
 
+contact_template_assoc = db.Table('contact_template_assoc' ,
+    db.Column('contact_id' , db.Integer , db.ForeignKey('contacts.id')),
+    db.Column('template_id' , db.Integer , db.ForeignKey('tempalate.id')),
+)
+
+user_projects_assoc = db.Table('user_projects_assoc' ,
+    db.Column('user_id' , db.Integer , db.ForeignKey('users.id')),
+    db.Column('project_id' , db.Integer , db.ForeignKey('project.id')),
+)
+
+contact_projects_assoc = db.Table('contact_projects_assoc' ,
+    db.Column('contact_id' , db.Integer , db.ForeignKey('contacts.id')),
+    db.Column('project_id' , db.Integer , db.ForeignKey('project.id')),
+)
+
+# Contact Template
+class contacts(db.Model):
+    __searchable__ = ['business_name' , 'contact_one' , 'contact_two' , 'city']
+    id = db.Column(db.Integer , primary_key = True)
+    business_name = db.Column(db.String(250) , nullable = False )
+    contact_one = db.Column(db.String(15))
+    contact_two = db.Column(db.String(15))
+    address = db.Column(db.String(250))
+    website = db.Column(db.String(100))
+    tag = db.Column(db.String(500))
+    city = db.Column(db.String(50))
+    wp_cnt = db.Column(db.Integer , default = 0)
+    sms_cnt = db.Column(db.Integer , default = 0)
+    email_cnt = db.Column(db.Integer , default = 0)
+    provider = db.Column(db.String(50))
+    url = db.Column(db.String(300))
+    link_hash = db.Column(db.String(150) , unique =True )
+    data_hash = db.Column(db.String(150) , unique = True )
+    keyword = db.Column(db.String(100))
+    # campaign / template ref -> Many to many
+    template =  db.relationship('template' , secondary= contact_template_assoc , backref= db.backref('contact' , lazy = 'dynamic'))
+    keyword_used = db.Column(db.String(200))
+
+# Template Model
 class template(db.Model):
     id = db.Column(db.Integer , primary_key = True)
     name = db.Column(db.String(100))
@@ -54,17 +93,36 @@ class template(db.Model):
     mssg_7 = db.Column(db.String(200) , default = "Jai Texart , Jaipur")
     mssg_8 = db.Column(db.String(200) , default = "+918875666619")
 
+
+# Users Model
+# Many to Many save
+# project =  Projects( ... add inputs ... )
+# project.users.append( user )
+
 class Users(db.Model):
     id = db.Column(db.Integer ,  primary_key = True) 
     username = db.Column(db.String(50))
     email = db.Column(db.String(50))
     password = db.Column(db.String(250))
-    # relation to projects
+    # relation to projects one to many
+    # Also for adding by admin , to a project 
+    projects = db.relationship('Project' , secondary= user_projects_assoc , backref= db.backref('users' , lazy = 'dynamic'))
+    flag_active = db.Column(db.Boolean , default = False)
+
+# Proejct Model
+# one to many
+# scrape_task(name="" , project_sc = <project object>)
+# db.session.add()
+# db.session.commit()
 
 class Project(db.Model):
-    pass 
-    # Table to hold projects
+    id = db.Column(db.Integer , primary_key = True)
+    name = db.Column(db.String(50))
+    contact = db.relationship('contacts' , secondary = contact_projects_assoc , backref=db.backref("project" , lazy= 'dynamic' ))
+    scrapers = db.relationship('scrape_task' , backref="project_sc")
+    jobs = db.relationship('job_task' , backref="project_jo")
 
+# Tasks Model
 class scrape_task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     city = db.Column(db.String(100))
@@ -74,6 +132,7 @@ class scrape_task(db.Model):
     timestamp = db.Column(db.DateTime, index=True , default=datetime.utcnow)
     meta = db.Column(db.String(500))
     # Project id it belongs to one to many
+    project = db.Column(db.Integer , db.ForeignKey('project.id'))
 
 class job_task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -83,5 +142,18 @@ class job_task(db.Model):
     timestamp = db.Column(db.DateTime, index=True , default=datetime.utcnow)
     keyword = db.Column(db.String(50))
     meta = db.Column(db.String(500) , default = str(0))
-    template =  db.relationship('template', backref='job_task', lazy=True) #One to one mapping
+    template =  db.relationship('template', backref='job_task', lazy= 'dynamic' ) #One to one mapping
     # Project id it belongs to one to many
+    project = db.Column(db.Integer , db.ForeignKey('project.id'))
+
+# Extra functionalities
+
+class import_file(FlaskForm):
+    data_file = FileField('data_file' , validators=[InputRequired()])
+
+class contact_search(FlaskForm):
+    search = StringField('search' , validators = [InputRequired()])
+
+class contact_filter(FlaskForm):
+    city = SelectMultipleField('city' , coerce =int)
+
