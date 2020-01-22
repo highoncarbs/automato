2efa6d58ca2badf5bc35816c7f7f775212090e4c
Padam Sethia <padamsethia5@gmail.com>
2020-01-22 15:34:01 +0530

    Nuked again

--- back/consumer.py
+++
@@ -1,5 +0,0 @@
-from google import huey 
-
-if __name__ == "__main__":
-    
-    print('Running')
--- back/run.py
+++
@@ -1,6 +0,0 @@
-from app import app
-from app import db
-
-if __name__ == "__main__":
-    db.create_all()
-    app.run(port= "5050" , debug = True)    
